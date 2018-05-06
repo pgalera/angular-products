729bfa82048eb3e4889318bf6b9ae13e9f67cc0c
Paco Galera <pgalera@ieseljust.com>
2018-05-06 19:32:00 +0200

    Semana1 ok

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'interfaces/i-product';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  title = "Mi lista de productos"
+  headers = {desc: 'Producto', price: 'Precio', avail: 'Disponible'};
+
+  products: IProduct[] = [{
+    id: 1,
+    desc: 'SSD hard drive',
+    avail: new Date('2016-10-03'),
+    price: 75,
+    imageUrl: 'assets/ssd.jpg',
+    rating: 5
+  },{
+    id: 2,
+    desc: 'LGA1151 Motherboard',
+    avail: new Date('2016-09-15'),
+    price: 96.95,
+    imageUrl: 'assets/motherboard.jpg',
+    rating: 4
+  }]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
