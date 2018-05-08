d2351272147e2e6cd397502373fa9e5b6167d4bf
Paco Galera <pgalera@ieseljust.com>
2018-05-08 20:48:27 +0200

    Semana2 ok y funcional

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,19 +14,33 @@ export class ProductListComponent implements OnInit {
 
   products: IProduct[] = [{
     id: 1,
-    desc: 'SSD hard drive',
+    desc: 'Disco duro SSD 240GB',
     avail: new Date('2016-10-03'),
     price: 75,
     imageUrl: 'assets/ssd.jpg',
     rating: 5
-  },{
+  }, {
     id: 2,
-    desc: 'LGA1151 Motherboard',
+    desc: 'Placa base LGA1151',
     avail: new Date('2016-09-15'),
     price: 96.95,
     imageUrl: 'assets/motherboard.jpg',
     rating: 4
-  }]
+  }, {
+    id: 3,
+    desc: 'Memoria DDR3 1600 8GB',
+    avail: new Date('2017-03-04'),
+    price: 85.95,
+    imageUrl: 'assets/ram.jpg',
+    rating: 3
+  }, {
+    id: 4,
+    desc: 'Disco duro mecánico 1TB',
+    avail: new Date('2015-12-11'),
+    price: 49.50,
+    imageUrl: 'assets/hdd.jpg',
+    rating: 2
+  }];
 
 
   toggleImage() {
